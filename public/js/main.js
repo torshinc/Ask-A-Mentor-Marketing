8bf8e37c81eb136f17804b184cca1f8e1f8f8224
David Sykora <dsykora@gmail.com>
2014-06-25 09:23:09 -0700

    Launch Day

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,41 @@
+//Message sent from iFrame when account is created and logged-in
 window.onmessage = function(e){
 	if (e.data == 'loginToTrainer') {
-		window.location = "https://www.ttrainer.com";
+		window.location = "http://app.askamentor.org";
+	}else if (e.data == 'showTerms') {
+		window.open("http://www.askamentor.org/terms.html",'_blank');
 	}
 };
+
+//Segment.io behaviour tracking
+$( ".find-mentor-button.top" ).click(function() {
+	analytics.track('Clicked Top Find A Mentor Button');
+});
+
+$( ".find-mentor-button.bottom" ).click(function() {
+	analytics.track('Clicked Bottom Find A Mentor Button');
+});
+
+$( ".learn-more-ntc" ).click(function() {
+	analytics.track('Clicked Learn More About NTC');
+});
+
+$( "#learnMore" ).click(function() {
+	analytics.track('Clicked Learn More About Torsh');
+});
+
+$( ".about-link" ).click(function() {
+	analytics.track('Clicked About Link');
+});
+
+$( ".what-is-link" ).click(function() {
+	analytics.track('Clicked What Is Link');
+});
+
+$( ".meet-link" ).click(function() {
+	analytics.track('Clicked Meet Mentors Link');
+});
+
+$( ".login" ).click(function() {
+	analytics.track('Clicked Login');
+});
