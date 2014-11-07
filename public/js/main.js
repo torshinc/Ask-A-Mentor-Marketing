67110fccab450718c1f6654b26188d19a9670384
David Sykora <dsykora@gmail.com>
2014-11-07 03:44:03 -0800

    Updates to public site

--- public/js/main.js
+++ public/js/main.js
@@ -39,3 +39,37 @@ $( ".meet-link" ).click(function() {
 $( ".login" ).click(function() {
 	analytics.track('Clicked Login');
 });
+
+$(document).ready(function(){
+	$('#signUpModalIframe').attr('src', 'http://app.askamentor.org/signup');
+	$('.mentors-carousel').slick({
+	  	slidesToShow: 4,
+  		slidesToScroll: 1,
+  		autoplay: true,
+  		autoplaySpeed: 7000,
+  		speed: 1200,
+  		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 886,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 680,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		]
+	});
+});
